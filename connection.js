7992d10e9114bcbd06e4aa32eee6bd071c17c9bb
Efty Mahmud <eftyraven@gmail.com>
2022-11-24 13:19:29 +0600

    first commit

--- connection.js
+++ connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s11qz.mongodb.net/chatAppMern?retryWrites=true&w=majority`, ()=> {
-  console.log('connected to mongodb')
-})
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.v4x91z2.mongodb.net/ChatApp?retryWrites=true&w=majority`,
+  () => {
+    console.log('connected to mongodb');
+  }
+);
